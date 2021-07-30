db8e05d717d39f4b81846c57060cabac7f811631
Yuushis <aryo.damar01@gmail.com>
2021-07-30 09:17:06 +0700

    add splashscreen

--- Selaru VR - 3D/Assets/Scripts/Scene Management/LevelManager.cs
+++ Selaru VR - 3D/Assets/Scripts/Scene Management/LevelManager.cs
@@ -5,6 +5,17 @@ using UnityEngine.SceneManagement;
 
 public class LevelManager: MonoBehaviour
 {
+    [SerializeField] private bool usingTimer;
+    [SerializeField] private float timer = 7f;
+    [SerializeField] private string nextSceneName; // next scene if using timer
+
+    private void Start()
+    {
+        if (usingTimer)
+        {
+            StartCoroutine(NextSceneByTimer());
+        }
+    }
 
     public void LoadSceneByName(string sceneName)
     {
@@ -16,4 +27,14 @@ public class LevelManager: MonoBehaviour
         SceneManager.LoadScene(sceneIndex);
     }
 
+    private void LoadUsingTimer()
+    {
+        SceneManager.LoadScene(nextSceneName);
+    }
+
+    IEnumerator NextSceneByTimer()
+    {
+        yield return new WaitForSeconds(timer);
+        LoadUsingTimer();
+    }
 }
